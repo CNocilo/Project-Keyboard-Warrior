681787b65a4201ee833b04facc4cc03ef9c02959
Ryan Buss <rybuss00@gmail.com>
2024-04-19 13:02:16 -0700

    rework file layout, modify models and add views

--- backend/Keyboard_Warrior_Project/settings.py
+++ backend/Keyboard_Warrior_Project/settings.py
@@ -44,7 +44,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -100,6 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+AUTH_USER_MODEL = "Keyboard_Warrior_app.GameUser"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
