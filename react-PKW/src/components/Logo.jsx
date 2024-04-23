eef15ffebe9a99fc64550b4c019138753c010027
CNocilo <cnocilo518@gmail.com>
2024-04-22 20:49:38 -0700

    did stuff

--- react-PKW/src/components/Logo.jsx
+++ react-PKW/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/temp_logo.png'
 
 const Logo = () => {
   return (
-    <section className="bg-[url('Project-Keyboard-Warrior\react-PKW\src\assets\An_blue_asian_landscape_painting_using_only_colors_around_hex_0e7490_hex_fef9c3_white_and_black_628335274.png')]">
+    <section>
         <img className="h-44 w-auto pt-5" src={ logo } alt="Keyboard Warriors"/>
         <span className="text-2xl pl-4 font-bold text-yellow-100 font-mono">
             Keyboard Warriors
--- react-PKW/src/pages/LoginPage.jsx
+++ react-PKW/src/pages/LoginPage.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const LoginPage = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+
+    }
+
+
   return (
     <section className="flex justify-center bg-cyan-400 font-mono">
         <div className="container-md m-[5%] p-5 bg-cyan-600 bg-opacity-50 rounded-md shadow-md">
@@ -12,24 +22,40 @@ const LoginPage = () => {
         <form>
             <div className="flex-col bg-yellow-100 p-5 m-[2%] rounded-md shadow-md">
                 <div className="m-[1%] p-[2%]">
-                    <label>
+                    <label htmlfor="username">
                         Username:
                     </label>
-                    <input className="border-2 border-gray-500 rounded-md focus:outline-cyan-500">
+                    <input className="border-2 border-gray-500 rounded-md focus:outline-cyan-500"
+                           id="username"
+                           type="text"
+                           value={username}
+                           onChange={(e) => setUsername(e.target.value)}>
                     
                     </input>
                 </div>
                 <div className="m-[1%] p-[2%]">
-                    <label>
+                    <label htmlfor="password">
                         Password:
                     </label>
-                    <input className="border-2 border-gray-500 rounded-md focus:outline-cyan-500">
+                    <input className="border-2 border-gray-500 rounded-md focus:outline-cyan-500"
+                           id="password"
+                           type="password"
+                           value={password}
+                           onChange={(e) => setPassword(e.target.value)}>
                     
                     </input>
                 </div>
-                <div className="m-[1%] mt-5 p-[2%]">
+                <div className="m-[1%] mt-3 p-[2%]">
                     <NavLink
                         to="/login"
+                        className="bg-cyan-400 rounded-md p-2 hover:text-white"
+                        >Login
+
+                    </NavLink>
+                </div>
+                <div className="m-[1%] mt-3 p-[2%]">
+                    <NavLink
+                        to="/test"
                         className="bg-cyan-400 rounded-md p-2 hover:text-white"
                         >Need to sign up?
 
