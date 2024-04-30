944fce4308b72a6d52761440e85702fbc4e16d41
CNocilo <cnocilo518@gmail.com>
2024-04-29 19:01:40 -0700

    working branch

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useState, useContext, useEffect } from 'react'
+import { AuthContext } from '../components/AuthContext'
 
 const UserPage = () => {
+  const [userData, setUserData] = useState("")
+  const { isAuthenticated, username } = useContext(AuthContext);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/api/userinfo?username=${username}`); // Replace with your API endpoint
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch user data');
+      }
+      const data = await response.json();
+      setUserData(data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData()
+  }, []);
+
   return (
-    <div>UserPage</div>
+    <section className="bg-cyan-400 p-[5%]">
+      <div className="bg-yellow-100 rounded-md p-[3%]">
+        <h1 className="font-mono text-lg">Username: {username}</h1>
+        
+
+      </div>
+
+    </section>
   )
 }
 
